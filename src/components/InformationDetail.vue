4a09edd8816e7121017254f796d4fdcaeb64be79
peakcary <peakcary@163.com>
2018-01-17 14:16:16 +0800

    01

--- src/components/InformationDetail.vue
+++ src/components/InformationDetail.vue
@@ -1,22 +1,48 @@
 <template>
-  <div class="time">
+<div class="information-detail">
+ <DetailHeader title={this.title} />
+ <div class="content">
+
     <div>2018-01-17</div>
 
   </div>
+</div>
+
 </template>
 
 <script>
+import * as axios from "axios";
+import DetailHeader from "./components/DetailHeader";
 export default {
-  name: "Time"
+  name: "InformationDetail",
+  components: {
+    DetailHeader
+  },
+  data() {
+    return {
+      title: ""
+    };
+  },
+  mounted() {
+    let self = this;
+    let id = this.$route.params.id;
+    axios
+      .get("http://operate.ptrcipo.com/news/" + id)
+      .then(function(res) {
+        console.log(res.data);
+        self.title = res.data.title;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.time {
-  height: 50px;
-  display: flex;
-  flex-direction: row ;
-  justify-content:space-between;
-  align-items: center;
+.information-detail {
+  .content {
+    padding: 1rem;
+  }
 }
 </style>
---
+++ src/components/components/DetailHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dtail-header">
+    <div class="left" @click="$router.go(-1)">
+      返回
+    </div>
+    <div class="center">
+      {{title}}
+    </div>
+    <div class="right">
+      分享
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DtailHeader",
+  props: ["title"]
+};
+</script>
+
+
+<style lang="scss" scoped>
+.dtail-header {
+  height: 2.5rem;
+  background: #28c9d8;
+  color: #fff;
+  font-size: 1rem;
+
+  padding: 0 1rem;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+
+
+  .left{
+
+  }
+  .center{
+    flex: 1;
+    display: flex;
+    justify-content: center;
+  }
+  .right{
+
+  }
+}
+</style>
